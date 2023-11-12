762fe7027b1145a4fcc0baa611bde8f0f6713dcc
sinYa Iwasaki <iwasaki@sinproject.net>
2023-11-12 16:16:32 +0900

    Show real client ip address #35

--- src/lib/client-ip.ts
+++ src/lib/client-ip.ts
@@ -5,10 +5,7 @@ export class ClientIP {
 	) {}
 
 	public get value(): string {
-		const client_ip = this._request.headers.get('x-forwarded-for') || this._get_client_address()
-
-		// console.log('x-forwarded-for', this._request.headers.get('x-forwarded-for'))
-		// console.log('get_client_address', this._get_client_address())
+		const client_ip = this._request.headers.get('x-forwarded-for') ?? this._get_client_address()
 
 		return client_ip
 	}
